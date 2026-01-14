eef9003ba352010fd0621b3295f3e43cbbdd6037
Roman Kozak <cossack.roman@gmail.com>
2026-01-14 13:01:30 +0200

    wip

--- modules/ui_components/selection_widget.py
+++ modules/ui_components/selection_widget.py
@@ -51,6 +51,8 @@ class SelectionScreen(ModalScreen[str | None]):
     }
     
     /* Highlight the selected item when list is focused */
+    ListView > ListItem.--highlight,
+    ListView:focus > ListItem.--highlight,
     ListView:focus > ListItem.-active {
         background: $primary;
         color: $background;
