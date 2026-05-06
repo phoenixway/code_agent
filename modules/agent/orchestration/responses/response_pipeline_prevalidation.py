0094cffd884708067c9e3c04e28671e8d51a408e
Roman Kozak <cossack.roman@gmail.com>
2026-05-07 02:54:55 +0300

    Keep compiler authority inventory consistent

--- modules/agent/orchestration/responses/response_pipeline_prevalidation.py
+++ modules/agent/orchestration/responses/response_pipeline_prevalidation.py
@@ -13,8 +13,13 @@ class ResponsePipelinePrevalidationMixin:
         "action_inside_think",
         "intent_inside_think",
         "file_content_inside_think",
+        "malformed_incomplete_file_content",
         "mixed_visible_text_and_control_protocol",
         "action_payload_array",
+        "action_payload_xml_fields",
+        "action_payload_tool_code",
+        "action_payload_not_object",
+        "protocol_tag_in_json_string",
         "multiple_actions",
         "file_content_must_follow_action",
     }
